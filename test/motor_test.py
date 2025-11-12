81f0fa15b8781fd60a8e18a3f400b0510ca695ce
sia's raspberry pi <artemis.sia.michael@gmail.com>
2025-11-12 14:14:35 -0500

    fix motor test

--- test/motor_test.py
+++ test/motor_test.py
@@ -5,32 +5,38 @@ import os
 import time
 import RPi.GPIO as GPIO
 
-RPWM_PIN = 22
+RPWM_PIN = 3
 LPWM_PIN = 4
 	
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RPWM_PIN, GPIO.OUT)
 GPIO.setup(LPWM_PIN, GPIO.OUT)
 
-print("set to low")
-GPIO.output(RPWM_PIN, GPIO.HIGH)
-# GPIO.output(LPWM_PIN, GPIO.LOW)
-time.sleep(0.5)
-
 pwm_forward = GPIO.PWM(RPWM_PIN, 1000)
 pwm_reverse = GPIO.PWM(LPWM_PIN, 1000)
 pwm_forward.start(0)
 pwm_reverse.start(0)
 try:
-	print("Spin forward", end="\n")
-	pwm_forward.ChangeDutyCycle(0)
+	print("Spin forward 10", end="\n")
+	pwm_forward.ChangeDutyCycle(10)
+	time.sleep(2)
+	
+	print("Spin forward 25", end="\n")
+	pwm_forward.ChangeDutyCycle(25)
+	time.sleep(2)
+
+	print("stop forward 50")
+	pwm_forward.ChangeDutyCycle(50)
+	time.sleep(1)
+	
+	print("Spin forward 100", end="\n")
+	pwm_forward.ChangeDutyCycle(100)
 	time.sleep(2)
 
 	print("stop forward")
-	pwm_forward.ChangeDutyCycle(100)
-	time.sleep(1)
+	pwm_forward.ChangeDutyCycle(0)
 	
-	"""
+	
 	print("Spin backwards", end="\n")
 	pwm_reverse.ChangeDutyCycle(100)
 	time.sleep(2)
@@ -38,7 +44,7 @@ try:
 	print("stop backwards")
 	pwm_reverse.ChangeDutyCycle(0)
 	time.sleep(1)
-	"""
+	
 	
 except KeyboardInterrupt:
 	print("Test ended")
